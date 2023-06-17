354701f148b4a78abd53a15c62362ab6b384ae81
dak1998 <32562277+dak1998@users.noreply.github.com>
2023-06-17 15:50:22 +0530

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.0"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.tuum"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.amqp:spring-rabbit-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
